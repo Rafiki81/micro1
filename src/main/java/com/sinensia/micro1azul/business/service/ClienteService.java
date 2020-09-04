26981c0f95a84623e62d2c0b03783b9ac0154252
rafa <rperezbeato@gmail.com>
2020-09-04 12:15:15 +0200

    Controlador y servicio de cliente

---
+++ src/main/java/com/sinensia/micro1azul/business/service/ClienteService.java
@@ -0,0 +1,10 @@
+package com.sinensia.micro1azul.business.service;
+import java.util.List;
+
+import com.sinensia.micro1azul.business.model.Cliente;
+
+public interface ClienteService {
+	
+	public List<Cliente> listarClientes();
+
+}
